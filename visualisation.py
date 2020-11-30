31ef0e58ef2e7536f7fd169da3104406a15f69d6
gerrie_vermeer <sander_vrijenhoek@hotmail.com>
2020-11-30 15:26:15 +0100

    Added grid

--- visualisation.py
+++ visualisation.py
@@ -1,11 +1,38 @@
 from simpylc import *
 
+normalFloorColor = (0, 0, 1)
+collisionFloorColor = (1, 0, 0.3)
+nrOfObstacles = 64
+
+
+class Floor(Beam):
+    side = 32
+    spacing = 0.2
+    halfSteps = round(0.5 * side / spacing)
+
+    class Stripe(Beam):
+        def __init__(self, **arguments):
+            super().__init__(size=(0.01, Floor.side, 0.01), **arguments)
+
+    def __init__(self, **arguments):
+        super().__init__(size=(self.side, self.side, 0.001), color=normalFloorColor)
+        self.xStripes = [self.Stripe(center=(0, nr * self.spacing, 0, 0), angle=90, color=(1, 1, 1)) for nr in
+                         range(-self.halfSteps, self.halfSteps)]
+        self.yStripes = [self.Stripe(center=(nr * self.spacing, 0, 0), color=(0, 0, 0)) for nr in
+                         range(-self.halfSteps, self.halfSteps)]
+
+    def __call__(self):
+        return super().__call__(color=collisionFloorColor if self.scene.collided else normalFloorColor, parts=lambda:
+        sum(xStripe() for xStripe in self.xStripes) +
+        sum(yStripe() for yStripe in self.yStripes))
+
 
 class Visualisation (Scene):
     def __init__(self):
         Scene.__init__(self)
 
         self.camera = Camera()
+        self.floor = Floor(scene=self)
 
         # Hull
         hull_color = (1, 1, 1)
@@ -31,17 +58,15 @@ class Visualisation (Scene):
         self.wind_vane = Beam(size=(0.05, 0.5, 0.05), center=(0, 0, 1.25), color=wind_vane_color)
         self.wind_vane_pointer = Cone(size=(0.15, 0.15, 0.15), center=(0, 0.25, 0), axis=(1, 0, 0), angle=-90, color=wind_vane_color)
 
-        # Ocean
-        water_color = (0, 0.025, 1)
-        self.ocean = Beam(size=(100, 100, 0.005), center=(0, 0, -0.5), color=water_color)
-
     def display(self):
-        sailboat_position = tEva((world.sailboat.position_x,  world.sailboat.position_y, world.sailboat.position_z))
+        sailboat_position = tEva((world.sailboat.position_x,  world.sailboat.position_y, world.sailboat.position_z + 0.5))
 
         self.camera(
-            position=tEva((world.sailboat.position_x,  world.sailboat.position_y, world.sailboat.position_z + 2.5)),
+            position=tEva((world.sailboat.position_x,  world.sailboat.position_y, world.sailboat.position_z + 3)),
             focus=tEva((world.sailboat.position_x + 0.00001,  world.sailboat.position_y, world.sailboat.position_z))
         )
+
+        self.floor()
 
         self.hull(
             position=sailboat_position,
@@ -51,8 +76,6 @@ class Visualisation (Scene):
                     rotation=world.sailboat.gimbal_rudder_angle,
                     parts=lambda:
                         self.rudder()
-                            
-                            
                     ) +
                 self.nose() +
                 self.rear() +
@@ -75,5 +98,3 @@ class Visualisation (Scene):
             parts=lambda:
                 self.wind_vane_pointer()
         )
-
-        self.ocean()
